28407ebcc06d3f9fc9b965b871fe2ff605747e68
Darren Richardson <mnestis@users.noreply.github.com>
2014-07-03 17:58:03 +0100

    A quick script, whilst I'm developing the provglish.transform code.

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import rdflib
+from provglish import transform
+from provglish import prov
+
+graph = rdflib.graph.ConjunctiveGraph()
+graph.parse("bravo.ttl",format="turtle", publicID="myGraph")
+prov.load_prov_ontology(graph)
+
+bindings = transform.definitions.bindings(graph)
+for binding in bindings:
+    print transform.definitions.make_string(binding)
+    triples = transform.definitions.coverage(binding, graph)
+    for triple in triples:
+        print "\t(%s, %s, %s)" % triple
+        
+print "\n\n"
+
+bindings = transform.properties.bindings(graph)
+for binding in bindings:
+    print transform.properties.make_string(binding)
+    triples = transform.properties.coverage(binding, graph)
+    for triple in triples:
+        print "\t(%s, %s, %s)" % triple
