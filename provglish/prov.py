a52b339355c3fa591394a8e1d4d3b8429bf2de82
Darren Richardson <mnestis@users.noreply.github.com>
2014-07-10 23:08:52 +0100

    Added pre-compiled SPARQL queries to provglish.prov
    This should hopefully speed things up a bit

--- provglish/prov.py
+++ provglish/prov.py
@@ -1,9 +1,35 @@
 import rdflib
+from rdflib.plugins import sparql
 
+less_precise_type_query = sparql.prepareQuery(
+    """SELECT ?lessPreciseType WHERE {
+        GRAPH <prov_graph> {
+            ?thing a ?thing_class .
+            ?thing a ?lessPreciseType
+        }
+        ?thing_class <http://www.w3.org/2000/01/rdf-schema#subClassOf>+ ?lessPreciseType
+    }"""
+)
+
+less_precise_prop_query = sparql.prepareQuery(
+    """SELECT ?lessPreciseProp WHERE {
+        GRAPH <prov_graph> {
+            ?thing1 ?prop ?thing2 .
+            ?thing1 ?lessPreciseProp ?thing2
+        }
+        ?prop <http://www.w3.org/2000/01/rdf-schema#subPropertyOf> ?lessPreciseProp
+    }"""
+)
 
 def load_prov_ontology(graph):
     graph.parse("http://www.w3.org/ns/prov.owl",format="xml")
     return graph
+
+def fetch_less_precise_type(thing, thing_class, graph):
+    return graph.query(less_precise_type_query, initBindings={"thing": thing, "thing_class": thing_class})
+
+def fetch_less_precise_prop(thing1, prop, thing2, graph):
+    return graph.query(less_precise_prop_query, initBindings={"thing1": thing1, "thing2": thing2, "prop": prop})
 
 def exists_more_precise(class_URI, subject_URI, graph):
     if graph.query("""SELECT ?morePreciseClass WHERE 
