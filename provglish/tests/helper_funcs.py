289bf16cfb335839b4ebbbb0601490f3a7860ed7
Darren Richardson <mnestis@users.noreply.github.com>
2014-07-17 09:55:30 +0100

    Created a new tests.helper_funcs.load_fixture function
    This is a more generic version of load_bravo.
    load_bravo has now been made a simple wrapper of this function.

--- provglish/tests/helper_funcs.py
+++ provglish/tests/helper_funcs.py
@@ -1,6 +1,9 @@
 import rdflib, os
 
 def load_bravo():
+    return load_fixture("bravo.ttl")
+
+def load_fixture(filename):
     graph = rdflib.graph.ConjunctiveGraph()
-    graph.parse(os.path.join(os.path.dirname(__file__), "test_fixtures/bravo.ttl"), format="turtle", publicID="prov_graph")
+    graph.parse(os.path.join(os.path.dirname(__file__), "test_fixtures/", filename), format="turtle", publicID="prov_graph")
     return graph
