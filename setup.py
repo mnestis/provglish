b9a4f36bb20a95a2019b10636727d30d6238fef9
Darren Richardson <mnestis@users.noreply.github.com>
2014-10-03 18:51:19 +0100

    Moved the nltk.downloader.download("all") command into the setup.py script
    This way it should be set up when the module is installed, rather than afterwards.

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.install import install
+
+class setup_nltk(install):
+	def run(self):
+		install.run(self)
+		import nltk	
+		print "  Downloading all the NLTK files. This may take some time..."
+                nltk.downloader.download("all")
+                print "    Done."
 
 setup(
 	name="Provglish",
@@ -9,7 +18,8 @@ setup(
 	scripts=["bin/prov-o-query","bin/prov2ce"],
 	license="LICENSE.txt",
 	description="Converting PROV to Controlled English",
-	long_desciption=open("README.md").read(),
+	long_description=open("README.md").read(),
+	cmdclass={"install": setup_nltk},
 	install_requires=[
 		"rdflib >= 4.1.2",
 		"inflect >= 0.2.4",
