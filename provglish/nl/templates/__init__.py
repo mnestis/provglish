ddb70b51333f2a99d22d35e31e22f8c1b17483d5
Darren Richardson <mnestis@users.noreply.github.com>
2014-11-04 12:21:34 +0000

    Added NL template : generation_activity

--- provglish/nl/templates/__init__.py
+++ provglish/nl/templates/__init__.py
@@ -1,3 +1,3 @@
 from entity_template import entity
 from activity_template import activity, activity_start, activity_end
-from generation_template import generation
+from generation_template import generation, generation_activity
--- provglish/nl/templates/generation_template.py
+++ provglish/nl/templates/generation_template.py
@@ -54,3 +54,60 @@ def _generation_string(bindings):
     return realise_sentence({"sentence":sentence})
 
 generation = transform.Template("Generation", _generation_binding, _generation_coverage, _generation_string)
+
+# This template deals with generations that have an activity, but no time
+
+_generation_act_query = sparql.prepareQuery(
+    """
+    SELECT ?entity ?generation ?activity WHERE {
+       GRAPH <prov_graph> {
+          {
+             ?entity a prov:Entity .
+             ?entity prov:qualifiedGeneration ?generation .
+             ?generation a prov:Generation .
+             ?generation prov:activity ?activity .
+             ?activity a prov:Activity
+          
+          } UNION {
+             ?entity a prov:Entity .
+             ?entity prov:wasGeneratedBy ?activity .
+             ?activity a prov:Activity
+          }
+       } 
+    }
+    """,
+    initNs={"prov":PROV})
+
+def _generation_act_binding(graph):
+    results = graph.query(_generation_act_query)
+    return results.bindings
+
+def _generation_act_coverage(bindings, graph):
+    if "?generation" in bindings:
+        return [(bindings["?entity"], RDF.type, PROV.Entity),
+                (bindings["?entity"], PROV.qualifiedGeneration, bindings["?generation"]),
+                (bindings["?generation"], RDF.type, PROV.Generation),
+                (bindings["?generation"], PROV.activity, bindings["?activity"]),
+                (bindings["?activity"], RDF.type, PROV.Activity)]
+    else:
+        return [(bindings["?entity"], RDF.type, PROV.Entity),
+                (bindings["?entity"], PROV.wasGeneratedBy, bindings["?activity"]),
+                (bindings["?activity"], RDF.type, PROV.Activity)]
+
+def _generation_act_string(bindings):
+    sentence = {}
+
+    sentence["subject"] = lex(bindings["?activity"])
+
+    sentence["object"] = {"type": "noun_phrase",
+                          "head": lex(bindings["?entity"]),
+                          "features": {"number": "plural" if plural_p(bindings["?entity"]) else "singular"}}
+    
+    sentence["verb"] = "generate"
+
+    sentence["features"] = {"passive":"true",
+                            "tense": "past"}
+
+    return realise_sentence({"sentence":sentence})
+
+generation_activity = transform.Template("Generation activity", _generation_act_binding, _generation_act_coverage, _generation_act_string)
