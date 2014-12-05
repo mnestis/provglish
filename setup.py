febfea93951fb88ee3a6a18439de5dcd03f73ad5
Darren Richardson <mnestis@users.noreply.github.com>
2014-12-05 14:32:35 +0000

    Made the nlgserv dependency rely on a specific revision (instead of greater than revision x)      in this case, specifically v0.2.3

--- setup.py
+++ setup.py
@@ -24,12 +24,12 @@ setup(
 		"rdflib >= 4.1.2",
 		"inflect >= 0.2.4",
 		"nltk >= 2.7.6",
-		"nlgserv >= 0.2.2",
+		"nlgserv == 0.2.3",
 	],
 	requires=[
 		"rdflib (>=4.1.2)",
 		"inflect (>=0.2.4)",
 		"nltk (>=2.7.6)",
-		"nlgserv (>=0.2.2)",
+		"nlgserv (==0.2.3)",
 	],
 )
